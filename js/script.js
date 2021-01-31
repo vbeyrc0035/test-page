02e2e8a38dd6d565a41d110852d0f5709542d404
harshkc03 <harshkc2000@gmail.com>
2021-01-31 23:48:10 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -1,27 +1,16 @@
-var piesiteFired = 0;
 $(document).ready(function() {
     var $win = $(window),
         $win_height = $(window).height(),
-        // - A multiple of viewport height - The higher this number the sooner triggered.
         windowPercentage = $(window).height() * 0.9;
     $win.on("click", scrollReveal);
     function scrollReveal() {
         var scrolled = $win.scrollTop();
-        
-        ///////////////////////////////////////
-        // Bar Charts scroll activate, looking for .trigger class to fire.
         $(".onAppear").each(function() {
             var $this = $(this),
                 offsetTop = $this.offset().top;
-
                 $(this).each(function(key, bar) {
                     var percentage = $(this).data("percentage");
-
-                    // $(this).css("animation-duration", 5);
                     $(this).css("height", percentage); 
-                    
-                    ///////////////////////////////////////
-                    //        Animated numbers
                     $(this).prop("Counter", 0).animate(
                         {
                             Counter: $(this).data("percentage")
@@ -34,13 +23,8 @@ $(document).ready(function() {
                             }
                         }
                     );
-                    //        Animated numbers
-                    ///////////////////////////////////////
                 });
-            
         });
-
     }
     scrollReveal();
 });
-
